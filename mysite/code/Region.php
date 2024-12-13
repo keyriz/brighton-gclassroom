59dbef3b26c4fe1fbaf3366b6a3b258d96c24263
Rizky Ramadhan <mo.rizrama@gmail.com>
2024-12-13 10:08:03 +0700

    Fix: Region - Setting slug

--- mysite/code/Region.php
+++ mysite/code/Region.php
@@ -5,6 +5,7 @@ class Region extends DataObject
 	private static $db = array(
 		'Title'       => 'Varchar',
 		'Description' => 'HTMLText',
+		'URLSegment'  => 'Varchar',
 	);
 
 	private static $has_one = array(
@@ -35,6 +36,7 @@ class Region extends DataObject
 	{
 		$fields = FieldList::create(
 			TextField::create('Title'),
+			TextField::create('URLSegment', 'URL Segment (Slug)')->setDisabled(true)->setAttribute('placeholder', 'Auto generate content'),
 			HtmlEditorField::create('Description'),
 			$upload = UploadField::create('Photo')
 		);
@@ -43,6 +45,31 @@ class Region extends DataObject
 		$upload->setFolderName('regions-photos');
 
 		return $fields;
+	}
+
+	public function onBeforeWrite()
+	{
+		parent::onBeforeWrite();
+
+		if (!$this->URLSegment || $this->URLSegment == 'new-region') {
+			$this->URLSegment = $this->generateURLSegment($this->Title);
+		} else {
+			$this->URLSegment = $this->generateURLSegment($this->URLSegment);
+		}
+
+		// Ensure uniqueness
+		$count    = 2;
+		$original = $this->URLSegment;
+		while (Property::get()->filter('URLSegment', $this->URLSegment)->exclude('ID', $this->ID)->exists()) {
+			$this->URLSegment = $original . '-' . $count;
+			$count++;
+		}
+	}
+
+	public function generateURLSegment($title)
+	{
+		$filter = URLSegmentFilter::create();
+		return $filter->filter($title);
 	}
 
 	public function LinkingMode()
@@ -63,6 +90,6 @@ class Region extends DataObject
 
 	public function Link()
 	{
-		return $this->RegionsPage()->Link('show/' . $this->ID);
+		return $this->RegionsPage()->Link('show/' . $this->URLSegment);
 	}
 }
