92809dd8ee56cd525a7a480161465e124fde5e02
Rizky Ramadhan <mo.rizrama@gmail.com>
2024-12-16 08:53:18 +0700

    Fix: Standarisasi PHP - Agent

--- mysite/code/AgentAdmin.php
+++ mysite/code/AgentAdmin.php
@@ -4,6 +4,6 @@ class AgentAdmin extends ModelAdmin
 {
 	private static $menu_title     = 'Agents';
 	private static $url_segment    = 'agents';
-	private static $managed_models = array('Agent');
+	private static $managed_models = array('AgentData');
 	private static $menu_icon      = 'mysite/icons/agent.png';
 }
